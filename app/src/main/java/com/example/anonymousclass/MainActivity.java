30fa3bdae35480b49c9074c357cfc752183373ea
JJW <ckjjw2953@naver.com>
2021-03-31 13:48:27 +0900

    Anonymous class

--- app/src/main/java/com/example/anonymousclass/MainActivity.java
+++ app/src/main/java/com/example/anonymousclass/MainActivity.java
@@ -19,13 +19,11 @@ public class MainActivity extends AppCompatActivity {
         mButton1 = findViewById(R.id.button1);
         mTextView1 = findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener());
-    }
-
-    private class MyOnClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            mTextView1.setText("You clicked button1");
-        }
+        mButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mTextView1.setText("You clicked button1");
+            }
+        });
     }
 }
